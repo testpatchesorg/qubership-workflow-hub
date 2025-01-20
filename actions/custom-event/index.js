4b2e3ec1eb8cb955a7840bc9312b964702c95606
Pavel Anikin <pavel.anikin@netcracker.com>
2025-01-20 16:27:20 +0400

    Add 2 custom actions

---
+++ actions/custom-event/index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    
+    const eventName = core.getInput('event_name', { required: true });
+    const clientPayloadInput = core.getInput('client_payload', { required: false }) || '{}';
+
+    let clientPayload;
+    try {
+      clientPayload = JSON.parse(clientPayloadInput);
+    } catch (error) {
+      throw new Error(`Invalid JSON for client_payload: ${clientPayloadInput}`);
+    }
+
+    core.info(`Event name: ${eventName}`);
+    core.info(`Client Payload: ${JSON.stringify(clientPayload)}`);
+
+    const token = process.env.GITHUB_TOKEN;
+    if (!token) {
+      throw new Error('GitHub token is not provided. Make sure it is passed as an environment variable.');
+    }
+    
+    const octokit = github.getOctokit(token);
+    const { owner, repo } = github.context.repo;
+
+    const response = await octokit.rest.repos.createDispatchEvent({
+      owner,
+      repo,
+      event_type: eventName,
+      client_payload: clientPayload,
+    });
+
+
+    core.setOutput('status', response.status);
+    core.info(`Custom event "${eventName}" triggered with status: ${response.status}`);
+    
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
