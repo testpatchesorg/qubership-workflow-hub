910b7e798985df443f7b4cf525c9be3a79f6065e
Pavel Anikin <snwteam5@gmail.com>
2025-05-15 10:05:19 +0400

    feat: update metadata action inputs with default values and optional chaining for configuration retrieval

--- actions/metadata-action/dist/index.js
+++ actions/metadata-action/dist/index.js
@@ -42847,8 +42847,8 @@ async function run() {
   const loader = new ConfigLoader()
   const config = loader.load(configurationPath, debug);
 
-  const defaultTemplate = core.getInput('default-template') || config["default-template"] || `{{ref-name}}-{{timestamp}}-{{runNumber}}`;
-  const defaultTag = core.getInput('default-tag') || config["default-tag"] || "latest";
+  const defaultTemplate = core.getInput('default-template') || config?.["default-template"] || `{{ref-name}}-{{timestamp}}-{{runNumber}}`;
+  const defaultTag = core.getInput('default-tag') || config?.["default-tag"] || "latest";
 
   //const defaultTemplate = core.getInput('default-template'); // || config["default-template"] || `{{ref-name}}-{{timestamp}}-{{runNumber}}`;
   //const defaultTag = core.getInput('defaut-tag'); //|| config["default-tag"] || "latest";
